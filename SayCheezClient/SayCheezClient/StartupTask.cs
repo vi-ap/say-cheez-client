16e4059d6f897aa3d90a0f38321d079ebf7bb8f9
vi-ap <aparna.23@outlook.com>
2016-07-02 12:11:56 -0700

    Removing for loop added for testing. Changing back to one picture taken from webcam. Changing folders location to SavedPictures instead of PicturesLibrary since the latter was throwing a null exception

--- SayCheezClient/SayCheezClient/StartupTask.cs
+++ SayCheezClient/SayCheezClient/StartupTask.cs
@@ -20,45 +20,41 @@ namespace SayCheezClient
         public void Run(IBackgroundTaskInstance taskInstance)
         {
             deferral = taskInstance.GetDeferral();
+            
             initialize();
-            for (int i=0; i<20; i++)
+            try
             {
-                Debug.WriteLine("Hi from Run method # " + i);
-                try
-                {
-                    takePhoto();
-                }
-                catch(Exception ex)
-                {
-                    Debug.WriteLine("In catch block");
-                    Debug.WriteLine(ex.Message);
-                }
-                Debug.WriteLine("At end of try/catch");
+                takePhoto();
             }
+            catch(Exception ex)
+            {
+                Debug.WriteLine("In catch block");
+                Debug.WriteLine(ex.Message);
+            }
+            Debug.WriteLine("At end of try/catch");
+            
         }
 
         private async void initialize()
         {
             Debug.WriteLine("Beginning of initialize");
             string photosFolderName = "SayCheez";
+            
+            IStorageItem folder = await KnownFolders.SavedPictures.TryGetItemAsync(photosFolderName);
 
-            mediaCapture = new MediaCapture();
-            await mediaCapture.InitializeAsync();
-
-            IStorageItem folder = await KnownFolders.PicturesLibrary.TryGetItemAsync(photosFolderName);
-
-            photosFolder = folder != null ? (StorageFolder)folder : await KnownFolders.PicturesLibrary.CreateFolderAsync(photosFolderName);
-            Debug.WriteLine("Line after creating photos folder. " + photosFolder.Path);
+            photosFolder = folder != null ? (StorageFolder)folder : await KnownFolders.SavedPictures.CreateFolderAsync(photosFolderName);
+            Debug.WriteLine("Line after creating/retrieving photos folder. " + photosFolder.Path);
 
         }
 
         private async void takePhoto()
-        {
-            
+        {            
             StorageFile photoFile;
 
+            mediaCapture = new MediaCapture();
+            await mediaCapture.InitializeAsync();
+
             Debug.WriteLine("Just before creating photo file");
-            
             photoFile = await photosFolder.CreateFileAsync(String.Format("{0:dd-MM-yyyy HH-mm-ss}.jpg", DateTime.Now), CreationCollisionOption.GenerateUniqueName);
             await mediaCapture.CapturePhotoToStorageFileAsync(ImageEncodingProperties.CreateJpeg(), photoFile);
 
